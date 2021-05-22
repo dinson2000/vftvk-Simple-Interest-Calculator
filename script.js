c40b2d5a0dea016eb056d6445ae9d7535f0716cf
dinson2000 <soni.dineshds2000@gmail.com>
2021-05-22 18:21:38 +0530

    Final Changes

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+computed=document.getElementById("computed");
+computed.style.display="none";
+function updateTextInput(val) {
+    document.getElementById('result').innerHTML=val+"%"; 
+  }
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    principal = document.getElementById("principal").value;
+    range = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    range=range/100;
+    amount=principal*(1+range*years)
+    amount1=amount-principal;
+    principal_populate = document.getElementById("principal_populate").innerHTML=principal;
+    rate_populate = document.getElementById("rate_populate").innerHTML=range*100;
+    amount_populate = document.getElementById("amount_populate").innerHTML=amount1;
+    year_populate = document.getElementById("year_populate").innerHTML=parseInt(years)+parseInt(new Date().getFullYear());
+
+
+    computed.style.display="block";
 }
         
